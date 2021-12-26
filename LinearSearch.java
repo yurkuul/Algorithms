926c232668ef335506355012def850043c652bab
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2021-12-26 15:02:50 -0600

    Create LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,15 @@
+public class LinearSearch() {
+  /**
+  * Searches for int target in int[] array and returns the index it is found at, if not found, then return -1.
+  *
+  * @return - the index at which the target is found at in the array
+  *
+  */
+  public static int search (int[] array, int target) {
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == target) {
+        return i;
+      }
+      return -1;
+  }
+}
