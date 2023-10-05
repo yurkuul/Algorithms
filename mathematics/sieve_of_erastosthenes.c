80e1c42e043acab64d14c0870dc52b70285b0f1f
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2023-10-05 11:44:08 -0500

    Update sieve_of_erastosthenes.c
    Added ability for user input and validation of user input.

--- mathematics/sieve_of_erastosthenes.c
+++ mathematics/sieve_of_erastosthenes.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+const int MAX_INT_VALUE = 50000000;
+
+/**
+ * Enumerated types for boolean values where false = 0, true = 1;
+*/
+typedef enum {
+    false,
+    true
+} BOOLEAN ;
 
 /**
  * Prints out all prime integers less than or equal to 'n' using the Sieve of
@@ -14,15 +25,35 @@ void sieve_of_erastosthenes(int n) {
 }
 
 int main() {
-    int maxVal;
+    int value;
+    BOOLEAN valid_input = false;
+    char continue_choice;
+    int buffer_value;
 
-    printf("Enter an integer to find all primes smaller than or equal to it: ");
+    while (!valid_input) {
+        printf("Enter an integer to find all primes smaller than or equal to it: ");
 
-    scanf("%d", &maxVal);
+        if (scanf("%d", &value) == 1) {
+            valid_input = true;
+        } else {
+            while ((buffer_value = getchar()) != '\n' && buffer_value != EOF);
+            printf("Error: Invalid Input.\n");
+            printf("\nContinue? Type any key to continue, or Q to quit: ");
+            scanf(" %c", &continue_choice);
 
-    printf("List of all prime integers less than or equal %d:\n", maxVal);
+            if (toupper(continue_choice) == 'Q') {
+                return 0;
+            }
+        }
 
-    sieve_of_erastosthenes(maxVal);
+        while ((buffer_value = getchar()) != '\n' && buffer_value != EOF);
+
+        printf("\n");
+    }
+
+    printf("List of all prime integers less than or equal to %d:\n", value);
+
+    sieve_of_erastosthenes(value);
 
     return 0;
 }
