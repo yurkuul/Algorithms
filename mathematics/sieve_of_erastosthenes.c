c90995b482c7ce92aa2e9e8213522f42550886cb
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2023-10-08 20:31:20 -0500

    Update sieve_of_erastosthenes.c

--- mathematics/sieve_of_erastosthenes.c
+++ mathematics/sieve_of_erastosthenes.c
@@ -10,7 +10,7 @@ const int MAX_INT_VALUE = 50000000;
 typedef enum {
     false,
     true
-} BOOLEAN ;
+} BOOLEAN;
 
 /**
  * Prints out all prime integers less than or equal to 'n' using the Sieve of
@@ -19,9 +19,37 @@ typedef enum {
  * @param n The max range for finding a list of prime integers.
 */
 void sieve_of_erastosthenes(int n) {
-    printf("[");
+    BOOLEAN prime_values[n + 1];
+    char array_str[10000];
+    int pos = 0;
+    array_str[pos++] = '[';
+    int array_size = sizeof(array_str);
 
-    printf("]\n");
+    for (int i = 0; i <= n; i++) {
+        prime_values[i] = true;
+    }
+
+    for (int p = 2; p * p <= n; p++) {
+        if (prime_values[p] == true) {
+            for (int i = p * p; i <= n; i += p) {
+                prime_values[i] = false;
+            }
+        }
+    }
+
+    if (n >= 2) {
+        pos += snprintf(array_str + pos, array_size - pos, "%d", 2);
+    }
+
+    for (int i = 3; i <= n; i++) {
+        if (prime_values[i] == true) {
+            pos += snprintf(array_str + pos, array_size - pos, ", %d", i);
+        }
+    }
+
+    pos += snprintf(array_str + pos, array_size - pos, "]\n");
+
+    printf("%s", array_str);
 }
 
 int main() {
@@ -33,8 +61,12 @@ int main() {
     while (!valid_input) {
         printf("Enter an integer to find all primes smaller than or equal to it: ");
 
-        if (scanf("%d", &value) == 1) {
-            valid_input = true;
+        if (scanf("%d", &value) == true) {
+            if (value >= 3) {
+                valid_input = true;
+            } else {
+                printf("Error: Input must be greater than or equal to 3.\n");
+            }
         } else {
             while ((buffer_value = getchar()) != '\n' && buffer_value != EOF);
             printf("Error: Invalid Input.\n");
